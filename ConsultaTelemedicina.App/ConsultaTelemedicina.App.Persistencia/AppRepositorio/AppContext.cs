15358b576fbe7253722125be78b579f4a72f4061
YudiGuayara <karinaguayara592@gmail.com>
2021-09-25 23:11:45 -0500

    Actualizando

---
+++ ConsultaTelemedicina.App/ConsultaTelemedicina.App.Persistencia/AppRepositorio/AppContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using ConsultaTelemedicina.App.Dominio;
+ namespace ConsultaTelemedicina.App.Persistencia
+{
+   public class AppContext : DbContext
+   {
+       public DbSet<Persona> Persona {get;set;}
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       {
+           if (!optionsBuilder.IsConfigured)
+           {
+               optionsBuilder
+               .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = ConsultaTelemedicinaData");
+           }
+       }
+   }   
+}
